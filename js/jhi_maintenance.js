545cb48bb8ac71d85664cfe2b902a0a72d160c38
Admin <admin@Admins-MacBook-Pro.local>
2019-12-07 13:28:11 -0500

    first commit

---
+++ js/jhi_maintenance.js
@@ -0,0 +1,18 @@
+// ***********************************************
+// JavaScript for Maintenance Page
+// Date: 06/08/16
+// Author: Shane McKinney (smckinney@jhancock.com)
+// Department: Digital Experience, JHI
+// ***********************************************
+
+
+// This JS file can be used for quick-fix
+// I.E. document.getElementById("DIV");
+// I.E. document.getElementById("DIV").innerHTML = "overwrite content";
+
+
+/***********************************************
+Load Default (alternative to <body onLoad="">
+***********************************************/
+
+<!-- window.onload=function(){document.getElementById("masofdate").innerHTML = '';}; -->
